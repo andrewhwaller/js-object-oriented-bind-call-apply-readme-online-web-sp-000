5cd4e3ddee09029a2b5c851ede7939f2377fbc0c
Andrew Waller <andrewhwaller@gmail.com>
2019-09-12 00:32:24 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
-function greet() {
-    console.log(`my name is ${this.name}, hi!`);
+function greet(customerOne, customerTwo) {
+    console.log(`Hi ${customerOne} and ${customerTwo}, my name is ${this.name}!`);
 }
 
 let sally = { name: 'Sally' };
