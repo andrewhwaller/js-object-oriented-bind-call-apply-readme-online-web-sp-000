24f00edaede4fe702ebb41c0937ee78609dba54e
Andrew Waller <andrewhwaller@gmail.com>
2019-09-12 00:33:35 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,8 +1,13 @@
-function greet(customerOne, customerTwo) {
-    console.log(`Hi ${customerOne} and ${customerTwo}, my name is ${this.name}!`);
-}
-
 let sally = { name: 'Sally' };
-
-
-greet.call(sally, 'Terry', 'George');
+ 
+function greet(customer) {
+    console.log(`Hi ${customer}, my name is ${this.name}!`);
+}
+ 
+let newGreet = greet.bind(sally); // newGreet is context-bound to sally
+ 
+newGreet('Bob');
+// Hi Bob, my name is Sally!
+ 
+greet('Bob');
+// Hi Bob, my name is !
